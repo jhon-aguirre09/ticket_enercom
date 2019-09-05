7955e8c8d90faaad778812fbce58a13c54117cba
Jhon_Aguirre <jhon.aguirre09@gmail.com>
2019-09-05 11:38:25 -0500

    Puesta en marcha

--- tickets/tickets/urls.py
+++ tickets/tickets/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accounts.urls', namespace='accounts')),
+    path('', include('django.contrib.auth.urls')),
+    path('test/',views.TestPage.as_view(), name='test'),
+    path('thanks/',views.ThanksPage.as_view(),name='thanks'),
 ]
